d5e376dec6a1f92ea6699377250e756400eb3b43
Jonathan Piat <jonathan.piat@qapa.com>
2014-04-24 08:11:26 +0200

    Update license

--- pgwcookie.js
+++ pgwcookie.js
@@ -4,7 +4,7 @@
  * Copyright 2014, Jonathan M. Piat
  * http://pgwjs.com - http://pagawa.com
  * 
- * Released under the MIT license - http://opensource.org/licenses/MIT
+ * Released under the GNU GPLv3 license - http://opensource.org/licenses/gpl-3.0
  */
 ;(function($){
     $.pgwCookie = function(obj) {
